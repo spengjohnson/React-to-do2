1a8c9c3aa3057cc6afb68e7ff9374900cae0b9da
spengjohnson <33362645+spengjohnson@users.noreply.github.com>
2018-04-21 14:55:14 -0600

    Update ToDo.js

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -5,7 +5,7 @@ class ToDo extends Component {
     return (
       <li>
       <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
-      <span>{ this.props.description }</span>
+      <span>{ this.props.description }{ this.props.deleteTodo }</span>
       </li>
     );
   }
